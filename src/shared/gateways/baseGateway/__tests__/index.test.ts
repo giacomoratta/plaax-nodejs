233294a33383c11f8475624e089b1730e890e2b7
giacomoratta <>
2022-11-28 08:26:43 +0100

    added base gateway with axios

---
+++ src/shared/gateways/baseGateway/__tests__/index.test.ts
@@ -0,0 +1,141 @@
+import axios, { AxiosError } from 'axios'
+import { BaseGateway } from '../index'
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+const createAxiosResponse = (extendResponse): object => {
+  return {
+    status: 200,
+    statusText: 'OK',
+    data: undefined,
+    headers: {},
+    ...extendResponse
+  }
+}
+
+const createAxiosError = (extendResponse): AxiosError => {
+  const error = new AxiosError()
+  Object.assign(error, {
+    message: 'axios error',
+    code: undefined,
+    config: undefined,
+    request: undefined,
+    response: {
+      status: 500,
+      statusText: 'Server error',
+      config: {},
+      data: undefined,
+      headers: {},
+      request: undefined,
+      ...extendResponse
+    }
+  })
+  return error
+}
+
+const axiosServerError500 = (): AxiosError => {
+  return createAxiosError({
+    status: 500,
+    statusText: 'Server error'
+  })
+}
+
+const TEST_SERVICE = {
+  url: 'http://gateway.test:8009/rest/v2',
+  timeout: 4000
+}
+
+class TestGateway extends BaseGateway {
+  constructor () {
+    super({
+      baseUrl: TEST_SERVICE.url,
+      queryParams: {
+        lang: 'nl'
+      },
+      timeout: TEST_SERVICE.timeout,
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    })
+  }
+
+  async getData (id: number): Promise<any> {
+    return await this.request({
+      method: 'GET',
+      endpoint: `/test-data/${id}`,
+      queryParams: {
+        sort: 'desc'
+      }
+    })
+  }
+}
+
+describe('BaseGateway', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('throws Gateway Error in case of unexpected error', async () => {
+    mockedAxios.isAxiosError.mockImplementationOnce(() => false)
+    mockedAxios.get.mockImplementationOnce(() => {
+      throw new Error('unexpected!')
+    })
+
+    const gateway = new TestGateway()
+    const execution = async (): Promise<object> => await gateway.getData(123456789)
+
+    await expect(execution()).rejects.toThrow()
+  })
+
+  it('gracefully handles an axios error with some response data', async () => {
+    mockedAxios.isAxiosError.mockReturnValueOnce(true)
+    mockedAxios.get.mockImplementationOnce(() => {
+      throw axiosServerError500()
+    })
+
+    const gateway = new TestGateway()
+    const execution = async (): Promise<object> => await gateway.getData(999000777)
+
+    await expect(execution()).resolves.toMatchObject({ status: 500, statusText: 'Server error' })
+  })
+
+  it('should call request method and axios method, and return axios response', async () => {
+    const gateway = new TestGateway()
+    mockedAxios.get.mockResolvedValueOnce(createAxiosResponse({
+      data: {
+        id: 123,
+        name: 'Bob'
+      }
+    }))
+    const gatewayRequest = jest.spyOn<any, string>(gateway, 'request')
+
+    const data = await gateway.getData(123)
+
+    expect(gatewayRequest).toHaveBeenCalledWith({
+      endpoint: '/test-data/123',
+      method: 'GET',
+      queryParams: {
+        sort: 'desc'
+      }
+    })
+    expect(axios.get).toHaveBeenCalledWith('http://gateway.test:8009/rest/v2/test-data/123', {
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      params: {
+        lang: 'nl',
+        sort: 'desc'
+      },
+      timeout: 4000
+    })
+    expect(data).toMatchObject({
+      data: {
+        id: 123,
+        name: 'Bob'
+      },
+      status: 200,
+      statusText: 'OK'
+    })
+  })
+})
---
+++ src/shared/gateways/baseGateway/index.ts
@@ -0,0 +1,96 @@
+import axios, { AxiosError } from 'axios'
+
+interface KeyValueObject {
+  [key: string]: string | number | boolean | null | undefined
+}
+
+interface BaseGatewayResponse {
+  status: number
+  statusText: string
+  data: any
+  headers: object
+}
+
+interface RequestConfig {
+  baseUrl: string
+  timeout?: number
+  queryParams?: KeyValueObject
+  headers?: KeyValueObject
+}
+
+export abstract class BaseGateway {
+  private readonly requestConfig: RequestConfig
+
+  protected constructor (requestConfig: RequestConfig) {
+    this.requestConfig = requestConfig
+  }
+
+  protected async request (requestLocalConfig: {
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE'
+    endpoint: string
+    queryParams?: KeyValueObject
+    headers?: KeyValueObject
+    body?: object | string | undefined
+  }): Promise<BaseGatewayResponse | undefined> {
+    const url = `${this.requestConfig.baseUrl}${requestLocalConfig.endpoint}`
+
+    const config = {
+      headers: this.buildRequestHeaders(requestLocalConfig.headers),
+      params: {
+        ...this.requestConfig.queryParams,
+        ...requestLocalConfig.queryParams
+      },
+      timeout: this.requestConfig.timeout
+    }
+
+    console.debug({ url, config }, 'BaseGateway request')
+
+    let axiosResponse
+    try {
+      if (requestLocalConfig.method === 'GET') {
+        axiosResponse = await axios.get(url, config)
+      } else if (requestLocalConfig.method === 'POST') {
+        axiosResponse = await axios.post(url, requestLocalConfig.body, config)
+      } else if (requestLocalConfig.method === 'PUT') {
+        axiosResponse = await axios.put(url, requestLocalConfig.body, config)
+      } else if (requestLocalConfig.method === 'DELETE') {
+        axiosResponse = await axios.delete(url, config)
+      }
+
+      return {
+        status: axiosResponse.status,
+        statusText: axiosResponse.statusText,
+        data: axiosResponse.data,
+        headers: axiosResponse.headers
+      }
+    } catch (error) {
+      return this.handleError(error)
+    }
+  }
+
+  private buildRequestHeaders (requestLocalConfigHeaders: KeyValueObject | undefined): object {
+    return {
+      'Content-Type': 'application/json',
+      ...this.requestConfig.headers,
+      ...requestLocalConfigHeaders
+    }
+  }
+
+  private handleError (error: AxiosError): BaseGatewayResponse | undefined {
+    if (!axios.isAxiosError(error) || error.response == null) {
+      const gatewayError = new Error('Unexpected error' + (error?.message === null ? '' : `: ${error.message}`))
+      gatewayError.name = 'Gateway Error'
+      console.error({ gatewayError, error })
+      throw gatewayError
+    }
+
+    console.warn({ errorResponse: error.response }, 'BaseGateway request error')
+
+    return {
+      status: error.response.status,
+      statusText: error.response.statusText,
+      data: error.response.data,
+      headers: error.response.headers
+    }
+  }
+}
