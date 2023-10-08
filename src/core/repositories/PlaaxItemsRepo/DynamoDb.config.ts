7262a7748bca5f02bbe013a186be872c477a4ad2
giacomoratta <90198+giacomoratta@users.noreply.github.com>
2023-10-08 10:28:06 +0200

    PLX-1009 fixed local server start with AWS credentials from env

--- src/core/repositories/PlaaxItemsRepo/DynamoDb.config.ts
+++ src/core/repositories/PlaaxItemsRepo/DynamoDb.config.ts
@@ -1,6 +1,18 @@
+import { DynamoDBClientConfig } from '@aws-sdk/client-dynamodb'
+
 const RESOURCE_PREFIX = process.env.RESOURCE_PREFIX ?? 'unknown'
 
-export const DynamoDBClientConfiguration = { region: 'eu-west-1' }
+const credentialsFromEnv = process.env.NODE_ENV !== 'production' && {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? ''
+  // sessionToken: ''
+  // expiration: '',
+}
+
+export const DynamoDBClientConfiguration: DynamoDBClientConfig = {
+  credentials: credentialsFromEnv || undefined,
+  region: process.env.AWS_DEFAULT_REGION ?? 'eu-west-1'
+}
 
 export const DynamoDbTables = {
   ITEMS: RESOURCE_PREFIX + '-items',
